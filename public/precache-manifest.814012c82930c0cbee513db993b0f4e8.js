2b5716705fe3223561c71855b4ad107a44b8d9b3
Lanette Lanting <lanettelanting@gmail.com>
2020-09-29 09:21:09 +0200

    before deploy

---
+++ public/precache-manifest.814012c82930c0cbee513db993b0f4e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac9f045d01f80619d44d6576f90aa00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71ef732b7fb516413041",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5ce584eba44e729d204b",
+    "url": "/static/css/main.41aed950.chunk.css"
+  },
+  {
+    "revision": "71ef732b7fb516413041",
+    "url": "/static/js/2.7206fb18.chunk.js"
+  },
+  {
+    "revision": "82e3c575391da7a71a1694351f425a33",
+    "url": "/static/js/2.7206fb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce584eba44e729d204b",
+    "url": "/static/js/main.a29e44e5.chunk.js"
+  },
+  {
+    "revision": "acc8a0a9c4d421dfb832",
+    "url": "/static/js/runtime-main.51102047.js"
+  },
+  {
+    "revision": "8547f1f50184d06943b0137821d47da2",
+    "url": "/static/media/bruno-emmanuelle-Gi6-m_t_W-E-unsplash.8547f1f5.jpg"
+  },
+  {
+    "revision": "16e57a37de923d3073e27f6403077577",
+    "url": "/static/media/markus-spiske-IQhjLO2uYmA-unsplash.16e57a37.jpg"
+  }
+]);
